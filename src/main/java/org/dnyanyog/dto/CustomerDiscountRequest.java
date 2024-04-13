a855da10f1f958452697a92da1be005985e09848
Sanket Yelam <iam.sanketyelam@gmail.com>
2024-04-13 19:30:19 +0530

    feat: Implement git convention check and formatter
     * Implement google java formatter
     * Add pre commit config file

---
+++ src/main/java/org/dnyanyog/dto/CustomerDiscountRequest.java
@@ -0,0 +1,23 @@
+package org.dnyanyog.dto;
+
+public class CustomerDiscountRequest {
+
+  private int age;
+  private char gender;
+
+  public int getAge() {
+    return age;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  public char getGender() {
+    return gender;
+  }
+
+  public void setGender(char gender) {
+    this.gender = gender;
+  }
+}
